ea3f0c516f4160ecffb1be3fb13a289aebc7ae4b
Abtahee Ali <abtaheeali@gmail.com>
2020-05-13 15:00:44 -0400

    Built a deck of cards

--- Program.cs
+++ Program.cs
@@ -9,12 +9,16 @@ namespace all_cards
         {
             var ranks = new List<string>() { "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace" };
             var suits = new List<string>() { "Clubs", "Spades", "Diamonds", "Hearts" };
+            var deck = new List<string>();
 
             for (var rankIndex = 0; rankIndex < ranks.Count; rankIndex++)
             {
                 for (var suitIndex = 0; suitIndex < suits.Count; suitIndex++)
                 {
-
+                    var currentRank = ranks[rankIndex];
+                    var currentSuit = suits[suitIndex];
+                    var currentCard = $"The {currentRank} of {currentSuit}";
+                    deck.Add(currentCard);
                 }
             }
         }
