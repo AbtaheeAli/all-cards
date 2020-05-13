ab980c2ae59571001357b727e31e4b2a7f553436
Abtahee Ali <abtaheeali@gmail.com>
2020-05-13 14:44:29 -0400

    for loops for ranks and suits

--- Program.cs
+++ Program.cs
@@ -7,9 +7,16 @@ namespace all_cards
     {
         static void Main(string[] args)
         {
-            var ranks = new List<string>()("two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace");
-            var suits = new List<string>()("Clubs", "Spades", "Diamonds", "Hearts");
+            var ranks = new List<string>() { "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace" };
+            var suits = new List<string>() { "Clubs", "Spades", "Diamonds", "Hearts" };
 
+            for (var rankIndex = 0; rankIndex < ranks.Count; rankIndex++)
+            {
+                for (var suitIndex = 0; suitIndex < suits.Count; suitIndex++)
+                {
+
+                }
+            }
         }
     }
 }
