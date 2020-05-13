cd3e1b362298af4092be42f251755d23c9a98539
Abtahee Ali <abtaheeali@gmail.com>
2020-05-13 14:22:14 -0400

    List of ranks and suits

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace all_cards
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var ranks = new List<string>()("two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace");
+            var suits = new List<string>()("Clubs", "Spades", "Diamonds", "Hearts");
+
+        }
     }
-  }
 }
