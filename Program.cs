f2982469472350241c83afe70e1f1c6e1c50eff1
Abtahee Ali <abtaheeali@gmail.com>
2020-05-13 16:55:26 -0400

    firstIndex and Random

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace all_cards
 {
@@ -7,13 +8,14 @@ namespace all_cards
     {
         static void Main(string[] args)
         {
-            var ranks = new List<string>() { "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King", "Ace" };
+            //declared ranks and suits for deck
+            var ranks = new List<string>() { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
             var suits = new List<string>() { "Clubs", "Spades", "Diamonds", "Hearts" };
             var deck = new List<string>();
 
-            for (var rankIndex = 0; rankIndex < ranks.Count; rankIndex++)
+            for (var rankIndex = 0; rankIndex < ranks.Count(); rankIndex++)
             {
-                for (var suitIndex = 0; suitIndex < suits.Count; suitIndex++)
+                for (var suitIndex = 0; suitIndex < suits.Count(); suitIndex++)
                 {
                     var currentRank = ranks[rankIndex];
                     var currentSuit = suits[suitIndex];
@@ -21,6 +23,24 @@ namespace all_cards
                     deck.Add(currentCard);
                 }
             }
+            var n = 52;
+            // for firstIndex from n - 1 down to 1 do:
+            for (var firstIndex = n - 1; firstIndex >= 1; firstIndex--)
+            {
+                Random rnd = new Random();
+                //   secondIndex = random integer that is greater than or equal to 0 and LESS than firstIndex
+                var secondIndex = rnd.Next(firstIndex);
+                //   Now swap the values at firstIndex and secondIndex by doing this:
+                //     firstValue = the value from items[firstIndex]
+                firstValue = deck[firstIndex];
+
+                //     secondValue = the value from items[secondIndex]
+                //     items[firstIndex] = secondValue
+                //     items[secondIndex] = firstValue
+            }
+
+
+
         }
     }
 }
